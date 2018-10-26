eba93e650427c1f9aba37a00572fcab47e34fc25
Kalandri <ali_ciftci@live.com>
2018-10-26 15:57:18 -0400

    Update PasswordHashing.py

--- PasswordHashing.py
+++ PasswordHashing.py
@@ -2,5 +2,5 @@ import hashlib, uuid
 
 def hashpass( password ):
   salt = uuid.uuid4().hex
-  hashed_password = hashlib.sha512(password + salt).hexdigest()
-  return {'hash':hashed_password, 'salt':salt}
+  hash = hashlib.sha512(password + salt).hexdigest()
+  return {'hash':hash, 'salt':salt}
