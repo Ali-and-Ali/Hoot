2a2e3bbe59799d9b6c74683b6b5e17da732eb216
bursalma <bursalma@gmail.com>
2018-10-25 22:05:11 -0400

    Database Access

--- LibraryManagement.py
+++ LibraryManagement.py
@@ -1,1 +1,28 @@
 from tkinter import *
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+  'user': 'frootyloops',
+  'password': 'Helvetica',
+  'host': 'the-library.cal2dzrrcm9n.us-east-2.rds.amazonaws.com',
+  'database': 'BookTable',
+  'raise_on_warnings': True
+}
+
+try:
+    con = mysql.connector.connect(**config)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    con.close()
+    print("Success")
+
+#window = Tk()
+
+#window.mainloop()
