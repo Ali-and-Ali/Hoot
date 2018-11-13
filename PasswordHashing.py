3e7df8c1584a974fdc4e1cb25f99a777d2445306
Kalandri <ali_ciftci@live.com>
2018-11-13 13:29:29 -0500

    Updated the password creation with API requests

--- PasswordHashing.py
+++ PasswordHashing.py
@@ -1,13 +1,19 @@
-import hashlib, uuid
+import hashlib, uuid, requests, json
 
 # This function is for hashing and salting the passwords of our users
-def createHash ( password ):
+def createHash ( password, username ):
   # First, we create a randomly generated salt to be used in the hashing process
   salt = uuid.uuid4().hex
   # Next, we encode both the password and the salt as utf-8 before we combine them together and create their hash
   hashed_pass = hashlib.sha512(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
-  # Finally, the salt that we used and the hash are both returned so that we can store them in the User table.
-  return {'hash':hashed_pass, 'salt':salt}
+  # We set the URL for our API here
+  API = "https://l11k4u8pyc.execute-api.us-east-2.amazonaws.com/Dev/"
+  # Here we create the request body that we will send to the API
+  params = {"username": username, "hash": hashed_pass, "salt": salt}
+  # Next, we send the request to the API and put the response information into the variable 'r'
+  r = requests.post(API, json.dumps(params))
+  # Finally, the status code is returned by the function
+  return {r.status_code}
 
 # This function helps us determine if the hash of the password that the user has entered is identical to the hash that we have store in our database
 def verifyPass ( password, results ):
