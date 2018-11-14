d27c7c9cd439fa0480913b52cba2c6f7cd04fd28
Kalandri <ali_ciftci@live.com>
2018-11-14 12:47:59 -0500

    Creating and Verifying User password is now all handled by API

--- PasswordHashing.py
+++ PasswordHashing.py
@@ -1,6 +1,5 @@
 import hashlib, uuid, requests, json
 
-# This function is for hashing and salting the passwords of our users
 def createHash ( password, username ):
   # First, we create a randomly generated salt to be used in the hashing process
   salt = uuid.uuid4().hex
@@ -15,15 +14,18 @@ def createHash ( password, username ):
   # Finally, the status code is returned by the function
   return {r.status_code}
 
-# This function helps us determine if the hash of the password that the user has entered is identical to the hash that we have store in our database
-def verifyPass ( password, results ):
-  # The results variable that we have as an argument in this function is actually a 2D list. The first item of the list (results[0][0])
-  # is the hash and the second item (results[0][1]) is the salt. So, we make a hash using the password supplied by the user and the salt
-  # that we kept from the user's registration process and then compare this hash with the one that we have stored in the database for the
-  # user
-  hashed_pass = hashlib.sha512(password.encode('utf-8') + results[0][1].encode('utf-8')).hexdigest()
-  # If the hash that we have created is identical to the one in the database, then we pass true else false.
-  if hashed_pass == results[0][0]:
-    return True
-  else:
-    return False
+def verifyUser ( password, username ):
+  API = "https://l11k4u8pyc.execute-api.us-east-2.amazonaws.com/Dev/salt"
+  params = {"username": username}
+  r = requests.post(API, json.dumps(params))
+
+  hashed_pass = hashlib.sha512(password.encode('utf-8') + r.json()['body'].replace('"', '').encode('utf-8')).hexdigest()
+
+  API = "https://l11k4u8pyc.execute-api.us-east-2.amazonaws.com/Dev/verifyuser"
+  params = {"username": username, "password": hashed_pass}
+  r = requests.post(API, json.dumps(params))
+  
+  return r.json()['body']
+
+print(createHash("France", "AliC"))
+print(verifyUser("France", "AliC"))
